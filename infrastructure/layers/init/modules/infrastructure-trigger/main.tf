268b1a06cddc0d45c98d3cf4e3038a24510bbf33
Sam Weeks <weekss@google.com>
2022-03-20 00:56:42 +0000

    adding a cloudbuild trigger for terraform destroy

--- infrastructure/layers/init/modules/infrastructure-trigger/main.tf
+++ infrastructure/layers/init/modules/infrastructure-trigger/main.tf
@@ -28,32 +28,32 @@ resource "google_cloudbuild_trigger" "apply" {
 
 }
 
-# Create Dev Cloud Build Trigger for destroy
-resource "google_cloudbuild_trigger" "destroy" {
+# # Create Dev Cloud Build Trigger for destroy
+# resource "google_cloudbuild_trigger" "destroy" {
 
-  name        = "infrastructure-layer-${var.layer-name}-dev-destroy-test"
-  description = "(Managed by Terraform - Do not manually edit) Infrastructure Layer ${var.layer-name} Dev Deployment"
-  project     = var.project
+#   name        = "infrastructure-layer-${var.layer-name}-dev-destroy-test"
+#   description = "(Managed by Terraform - Do not manually edit) Infrastructure Layer ${var.layer-name} Dev Deployment"
+#   project     = var.project
 
-  substitutions = {
-    _DEPLOY_PROJECT_ = var.dev-project
-    _ENVIRONMENT_    = "dev"
-    _LAYER_NAME_     = var.layer-name
-  }
+#   substitutions = {
+#     _DEPLOY_PROJECT_ = var.dev-project
+#     _ENVIRONMENT_    = "dev"
+#     _LAYER_NAME_     = var.layer-name
+#   }
 
-  source_to_build {
-    repo_type   = "CLOUD_SOURCE_REPOSITORIES"
-    ref = "refs/heads/master"
-    uri  = var.cloud-source-repositories-repo-uri
-  }
+#   source_to_build {
+#     repo_type   = "CLOUD_SOURCE_REPOSITORIES"
+#     ref = "refs/heads/master"
+#     uri  = var.cloud-source-repositories-repo-uri
+#   }
 
-  git_file_source {
-    path      = var.cloudbuild-destroy-config-path
-    uri       = var.cloud-source-repositories-repo-uri
-    revision  = "refs/heads/master"
-    repo_type = "CLOUD_SOURCE_REPOSITORIES"
-  }
+#   git_file_source {
+#     path      = var.cloudbuild-destroy-config-path
+#     uri       = var.cloud-source-repositories-repo-uri
+#     revision  = "refs/heads/master"
+#     repo_type = "CLOUD_SOURCE_REPOSITORIES"
+#   }
    
-  # filename = var.cloudbuild-destroy-config-path
+#   # filename = var.cloudbuild-destroy-config-path
 
-}
+# }
