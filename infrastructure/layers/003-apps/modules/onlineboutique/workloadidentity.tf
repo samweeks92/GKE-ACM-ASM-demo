1d2235fbd7cd831e39f1569537502430e45ede2e
Sam Weeks <weekss@google.com>
2022-10-15 14:10:36 +0100

    adding WI bindings

--- infrastructure/layers/003-apps/modules/onlineboutique/workloadidentity.tf
+++ infrastructure/layers/003-apps/modules/onlineboutique/workloadidentity.tf
@@ -1,10 +1,19 @@
 resource "google_service_account_iam_binding" "workload-identity-binding" {
-  for_each = var.onlineboutique-namespaces
   
   service_account_id = "projects/${var.project}/serviceAccounts/${var.cluster-sa}"
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
-    "serviceAccount:${var.project}.svc.id.goog[${each.value}/${each.value}]",
+    "serviceAccount:${var.project}.svc.id.goog[ad/ad]",
+    "serviceAccount:${var.project}.svc.id.goog[cart/cart]",
+    "serviceAccount:${var.project}.svc.id.goog[checkout/checkout]",
+    "serviceAccount:${var.project}.svc.id.goog[currency/currency]",
+    "serviceAccount:${var.project}.svc.id.goog[email/email]",
+    "serviceAccount:${var.project}.svc.id.goog[frontend/frontend]",
+    "serviceAccount:${var.project}.svc.id.goog[loadgenerator/loadgenerator]",
+    "serviceAccount:${var.project}.svc.id.goog[payment/payment]",
+    "serviceAccount:${var.project}.svc.id.goog[product-catalog/product-catalog]",
+    "serviceAccount:${var.project}.svc.id.goog[recommendation/recommendation]",
+    "serviceAccount:${var.project}.svc.id.goog[shipping/shipping]"
   ]
 }
