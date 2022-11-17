c4ffbbb25de3032582a452dae35a843a4d8519c3
Sam Weeks <weekss@google.com>
2022-11-17 17:04:36 +0000

    tidy up

--- infrastructure/layers/002-cluster/modules/private-cluster/config-connector.tf
+++ infrastructure/layers/002-cluster/modules/private-cluster/config-connector.tf
@@ -12,17 +12,17 @@ resource "google_service_account" "config_connector_service_account" {
 resource "google_project_iam_member" "config_connector_project_editor" {
   count        = var.enable_config_connector ? 1 : 0
     
-  project = google_service_account.config_connector_service_account.project
+  project = google_service_account.config_connector_service_account[0].project
   role    = "roles/editor"
-  member  = "serviceAccount:${google_service_account.config_connector_service_account.email}"
+  member  = "serviceAccount:${google_service_account.config_connector_service_account[0].email}"
 }
 
 resource "google_service_account_iam_member" "config_connector_wi_user" {
   count        = var.enable_config_connector ? 1 : 0
   
   role    = "roles/iam.workloadIdentityUser"
-  service_account_id = google_service_account.config_connector_service_account.name
-  member  = "serviceAccount:${google_service_account.config_connector_service_account.project}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
+  service_account_id = google_service_account.config_connector_service_account[0].name
+  member  = "serviceAccount:${google_service_account.config_connector_service_account[0].project}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
 }
 
 # Create the config-connector config
@@ -36,7 +36,7 @@ resource "null_resource" "config-connector" {
   }
     
   provisioner "local-exec" {
-    command = "sed -i -e 's/SERVICEACCOUNTPLACEHOLDER/${google_service_account.config_connector_service_account.email}/g' ${path.module}/resources/config-connector.yaml && kubectl apply -f ${path.module}/resources/config-connector.yaml"
+    command = "sed -i -e 's/SERVICEACCOUNTPLACEHOLDER/${google_service_account.config_connector_service_account[0].email}/g' ${path.module}/resources/config-connector.yaml && kubectl apply -f ${path.module}/resources/config-connector.yaml"
   }
 
 }
