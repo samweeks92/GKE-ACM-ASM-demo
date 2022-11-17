a7c750df141bd1ee3c3d88bb5d1a6bbd496b4da5
Sam Weeks <weekss@google.com>
2022-11-17 17:14:01 +0000

    tidy up

--- infrastructure/layers/002-cluster/modules/anthos-features/acm.tf
+++ infrastructure/layers/002-cluster/modules/anthos-features/acm.tf
@@ -1,36 +1,36 @@
-resource "google_gke_hub_feature" "configmanagement_acm_feature" {
-  count    = var.enable_acm_feature ? 1 : 0
-  name     = "configmanagement"
-  project  = var.project_id
-  location = "global"
-  provider = google-beta
+# resource "google_gke_hub_feature" "configmanagement_acm_feature" {
+#   count    = var.enable_acm_feature ? 1 : 0
+#   name     = "configmanagement"
+#   project  = var.project_id
+#   location = "global"
+#   provider = google-beta
 
-    depends_on = [google_gke_hub_membership.membership]
-}
+#     depends_on = [google_gke_hub_membership.membership]
+# }
  
  
-resource "google_gke_hub_feature_membership" "feature_member" {
-  provider   = google-beta
-  location   = "global"
-  feature    = "configmanagement"
-  membership = google_gke_hub_membership.membership[0].membership_id
-  configmanagement {
-    version = "1.13.1"
+# resource "google_gke_hub_feature_membership" "feature_member" {
+#   provider   = google-beta
+#   location   = "global"
+#   feature    = "configmanagement"
+#   membership = google_gke_hub_membership.membership[0].membership_id
+#   configmanagement {
+#     version = "1.13.1"
  
-    config_sync {
-      source_format = "unstructured"
-      git {
-        sync_repo   = var.sync_repo
-        policy_dir = var.policy_dir
-        secret_type = var.secret_type
-        sync_branch = var.sync_branch
-      }
-    }
-    policy_controller {
-      enabled                    = true
-      template_library_installed = true
-      referential_rules_enabled  = true
-    }
-  }
+#     config_sync {
+#       source_format = "unstructured"
+#       git {
+#         sync_repo   = var.sync_repo
+#         policy_dir = var.policy_dir
+#         secret_type = var.secret_type
+#         sync_branch = var.sync_branch
+#       }
+#     }
+#     policy_controller {
+#       enabled                    = true
+#       template_library_installed = true
+#       referential_rules_enabled  = true
+#     }
+#   }
 
-}
+# }
--- infrastructure/layers/002-cluster/modules/anthos-features/asm.tf
+++ infrastructure/layers/002-cluster/modules/anthos-features/asm.tf
@@ -1,91 +1,91 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+# /**
+#  * Copyright 2022 Google LLC
+#  *
+#  * Licensed under the Apache License, Version 2.0 (the "License");
+#  * you may not use this file except in compliance with the License.
+#  * You may obtain a copy of the License at
+#  *
+#  *      http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  */
 
-locals {
-  // GKE release channel is a list with max length 1 https://github.com/hashicorp/terraform-provider-google/blob/9d5f69f9f0f74f1a8245f1a52dd6cffb572bbce4/google/resource_container_cluster.go#L954
-  gke_release_channel          = data.google_container_cluster.asm.release_channel != null ? data.google_container_cluster.asm.release_channel[0].channel : ""
-  gke_release_channel_filtered = lower(local.gke_release_channel) == "unspecified" ? "" : local.gke_release_channel
-  // In order or precedence, use (1) user specified channel, (2) GKE release channel, and (3) regular channel
-  channel       = lower(coalesce(var.channel, local.gke_release_channel_filtered, "regular"))
-  revision_name = "asm-managed${local.channel == "regular" ? "" : "-${local.channel}"}"
-  // Fleet ID should default to project ID if unset
-  fleet_id = coalesce(var.fleet_id, var.project_id)
-}
-
-data "google_container_cluster" "asm" {
-  project  = var.project_id
-  name     = var.cluster_name
-  location = var.cluster_location
-}
-
-# resource "kubernetes_namespace" "system" {
-#   metadata {
-#     name = "istio-system"
-#   }
+# locals {
+#   // GKE release channel is a list with max length 1 https://github.com/hashicorp/terraform-provider-google/blob/9d5f69f9f0f74f1a8245f1a52dd6cffb572bbce4/google/resource_container_cluster.go#L954
+#   gke_release_channel          = data.google_container_cluster.asm.release_channel != null ? data.google_container_cluster.asm.release_channel[0].channel : ""
+#   gke_release_channel_filtered = lower(local.gke_release_channel) == "unspecified" ? "" : local.gke_release_channel
+#   // In order or precedence, use (1) user specified channel, (2) GKE release channel, and (3) regular channel
+#   channel       = lower(coalesce(var.channel, local.gke_release_channel_filtered, "regular"))
+#   revision_name = "asm-managed${local.channel == "regular" ? "" : "-${local.channel}"}"
+#   // Fleet ID should default to project ID if unset
+#   fleet_id = coalesce(var.fleet_id, var.project_id)
 # }
 
-resource "google_gke_hub_feature" "mesh" {
-  count    = var.enable_mesh_feature ? 1 : 0
-  name     = "servicemesh"
-  project  = var.project_id
-  location = "global"
-  provider = google-beta
+# data "google_container_cluster" "asm" {
+#   project  = var.project_id
+#   name     = var.cluster_name
+#   location = var.cluster_location
+# }
 
-  depends_on = [google_gke_hub_membership.membership]
-}
+# # resource "kubernetes_namespace" "system" {
+# #   metadata {
+# #     name = "istio-system"
+# #   }
+# # }
 
-# Run this local-exec on every single run to configure the fleet membership for managed ASM
-resource "null_resource" "managed-asm-control-plane" {
+# resource "google_gke_hub_feature" "mesh" {
+#   count    = var.enable_mesh_feature ? 1 : 0
+#   name     = "servicemesh"
+#   project  = var.project_id
+#   location = "global"
+#   provider = google-beta
 
-  depends_on = [google_gke_hub_feature.mesh]
+#   depends_on = [google_gke_hub_membership.membership]
+# }
 
-  triggers = {
-    always_run = timestamp()
-  }
+# # Run this local-exec on every single run to configure the fleet membership for managed ASM
+# resource "null_resource" "managed-asm-control-plane" {
 
-  provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials ${var.cluster_name} --region=${var.cluster_location} --project=${var.project_id} && gcloud container fleet mesh update --control-plane automatic --memberships ${var.cluster_name}-membership --project ${var.project_id}"
-  }
+#   depends_on = [google_gke_hub_feature.mesh]
 
-}
+#   triggers = {
+#     always_run = timestamp()
+#   }
 
-resource "kubernetes_config_map" "asm_options" {
-  metadata {
-    name      = "asm-options"
-    #namespace = kubernetes_namespace.system.metadata[0].name
-    namespace = "istio-system"
-  }
+#   provisioner "local-exec" {
+#     command = "gcloud container clusters get-credentials ${var.cluster_name} --region=${var.cluster_location} --project=${var.project_id} && gcloud container fleet mesh update --control-plane automatic --memberships ${var.cluster_name}-membership --project ${var.project_id}"
+#   }
 
-  data = {
-    multicluster_mode = var.multicluster_mode
-  }
+# }
 
-  depends_on = [google_gke_hub_membership.membership, google_gke_hub_feature.mesh]
-}
+# resource "kubernetes_config_map" "asm_options" {
+#   metadata {
+#     name      = "asm-options"
+#     #namespace = kubernetes_namespace.system.metadata[0].name
+#     namespace = "istio-system"
+#   }
 
-module "cpr" {
-  source  = "terraform-google-modules/gcloud/google//modules/kubectl-wrapper"
-  version = "~> 3.1"
+#   data = {
+#     multicluster_mode = var.multicluster_mode
+#   }
 
-  project_id       = var.project_id
-  cluster_name     = var.cluster_name
-  cluster_location = var.cluster_location
+#   depends_on = [google_gke_hub_membership.membership, google_gke_hub_feature.mesh]
+# }
 
-  kubectl_create_command  = "${path.module}/scripts/create_cpr.sh ${local.revision_name} ${local.channel} ${var.enable_cni} ${var.enable_vpc_sc}"
-  kubectl_destroy_command = "${path.module}/scripts/destroy_cpr.sh ${local.revision_name}"
+# module "cpr" {
+#   source  = "terraform-google-modules/gcloud/google//modules/kubectl-wrapper"
+#   version = "~> 3.1"
 
-  module_depends_on = [kubernetes_config_map.asm_options]
-}
+#   project_id       = var.project_id
+#   cluster_name     = var.cluster_name
+#   cluster_location = var.cluster_location
+
+#   kubectl_create_command  = "${path.module}/scripts/create_cpr.sh ${local.revision_name} ${local.channel} ${var.enable_cni} ${var.enable_vpc_sc}"
+#   kubectl_destroy_command = "${path.module}/scripts/destroy_cpr.sh ${local.revision_name}"
+
+#   module_depends_on = [kubernetes_config_map.asm_options]
+# }
