4b91340525638c9f1240d0cf0e39ef82af6894ef
Sam Weeks <weekss@google.com>
2022-11-17 09:53:34 +0000

    enabling config connector

--- infrastructure/layers/002-cluster/modules/private-cluster/cluster.tf
+++ infrastructure/layers/002-cluster/modules/private-cluster/cluster.tf
@@ -398,13 +398,19 @@ resource "google_project_iam_member" "config_connector_project_editor" {
 
 resource "google_service_account_iam_member" "config_connector_wi_user" {
   role    = "roles/iam.workloadIdentityUser"
-  service_account_id = google_service_account.config_connector_service_account.email
+  service_account_id = google_service_account.config_connector_service_account.name
   member  = "serviceAccount:${google_service_account.config_connector_service_account.project}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
 }
 
 # Create the config-connector config
 resource "null_resource" "config-connector" {
 
+  depends_on = [google_container_cluster.primary]
+
+  triggers = {
+    always_run = timestamp()
+  }
+    
   provisioner "local-exec" {
     command = "sed -i -e 's/SERVICEACCOUNT/${google_service_account.config_connector_service_account.email}/g' ${path.module}/resources/config-connector.yaml && kubectl apply -f ${path.module}/resources/config-connector.yaml"
   }
