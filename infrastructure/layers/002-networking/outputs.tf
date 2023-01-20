6c437e484e509ae80d5c56f2f6d43c825f361e83
Sam Weeks <weekss@google.com>
2023-01-20 23:13:12 +0900

    creating project creation automation

--- infrastructure/layers/002-networking/outputs.tf
+++ infrastructure/layers/002-networking/outputs.tf
@@ -23,3 +23,13 @@ output "ip-range-pods" {
 output "ip-range-services" {
   value = google_compute_subnetwork.subnet.secondary_ip_range[1].ip_cidr_range
 }
+
+// Output the name of the Pods secondary range
+output "ip-range-pods-name" {
+  value = google_compute_subnetwork.subnet.secondary_ip_range[0].range_name
+}
+
+// Output the name of the Services secondary range
+output "ip-range-services-name" {
+  value = google_compute_subnetwork.subnet.secondary_ip_range[1].range_name
+}
