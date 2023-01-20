a3fb3a005eb9dd05b4c0f47b068cda2e93bda18b
Sam Weeks <weekss@google.com>
2023-01-20 20:06:37 +0900

    creating project creation automation

--- infrastructure/layers/001-bootstrap/modules/create-projects/main.tf
+++ infrastructure/layers/001-bootstrap/modules/create-projects/main.tf
@@ -8,10 +8,10 @@ data "google_project" "cicd-project" {
   project_id = var.cicd-project
 }
 
-data "google_folder" "folder" {
-  folder              = "folders/${data.google_project.cicd-project.folder_id}"
-  lookup_organization = true
-}
+# data "google_folder" "folder" {
+#   folder              = "folders/${data.google_project.cicd-project.folder_id}"
+#   lookup_organization = true
+# }
 
 resource "google_project" "create-project" {
   name       = var.project
@@ -28,7 +28,7 @@ resource "google_project_iam_member" "cb-permissions" {
 }
 
 resource "google_folder_iam_member" "cb-permissions-shared-vpc" {
-  folder = "folders/${data.google_folder.folder.name}"
+  folder = "folders/${data.google_project.cicd-project.folder_id}"
   role    = "roles/compute.xpnAdmin"
   member  = "serviceAccount:${data.google_project.cicd-project.number}@cloudbuild.gserviceaccount.com"
 }
